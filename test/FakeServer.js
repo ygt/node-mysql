05809064d22b5d9f75bae0169bbb1030a74a590a
Jamie Brough <jamie1612@gmail.com>
2015-03-11 13:58:48 +0000

    log all statements made on first connection

--- test/FakeServer.js
+++ test/FakeServer.js
@@ -22,7 +22,14 @@ function FakeServer(options) {
 
   this._server      = null;
   this._connections = [];
+
 }
+
+FakeServer.prototype.statements = function() {
+  return this._connections.map(function(conn) {
+    return conn.statements;
+  })[0];
+};
 
 FakeServer.prototype.listen = function(port, cb) {
   this._server = Net.createServer(this._handleConnection.bind(this));
@@ -65,6 +72,9 @@ function FakeConnection(socket) {
   this._handshakeOptions              = {};
 
   socket.on('data', this._handleData.bind(this));
+
+  this.statements = [];
+
 }
 
 FakeConnection.prototype.handshake = function(options) {
@@ -118,6 +128,8 @@ FakeConnection.prototype._handleQueryPacket = function _handleQueryPacket(packet
   var conn = this;
   var match;
   var sql = packet.sql;
+
+  this.statements.push(sql);
 
   if ((match = /^START TRANSACTION|^UPDATE |^INSERT |^COMMIT|^ROLLBACK/i.exec(sql))) {
     var num = match[1];
